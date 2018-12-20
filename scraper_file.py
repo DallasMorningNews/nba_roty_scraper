fcdacffe0fc3b390764922815e7ad53383d1c9c6
jdhancock88 <jdhancock@gmail.com>
2018-12-20 17:04:51 -0600

    Alters scraper to include all metrics. Was leaving out minutes per game.

--- scraper_file.py
+++ scraper_file.py
@@ -31,7 +31,7 @@ HEADERS = {
 # base data dictionary. We'll iterate over the data from basketball-reference (BR)
 # and parse the data for players and stats and add it to this dictionary
 rookie_data = {
-    "metrics": ["winshare", "per", "usg", "vorp", "ppg", "rpg", "apg", "spg", "bpg"],
+    "metrics": ["winshare", "per", "usg", "bpm", "mpg", "ppg", "rpg", "apg", "spg", "bpg"],
     "stats": [],
     "players": [],
     "updated": time_ran
@@ -80,7 +80,150 @@ def set_stats(data):
 
 # we define our inital set of players by hitting the NBA's api for rookie stats
 
-def get_player_set(year):
+def get_player_set():
+
+    first_100 = requests.get("https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fplay-index%2Fpsl_finder.cgi%3Frequest%3D1%26match%3Dsingle%26type%3Dtotals%26per_minute_base%3D36%26per_poss_base%3D100%26season_start%3D1%26season_end%3D1%26lg_id%3DNBA%26age_min%3D0%26age_max%3D99%26is_playoffs%3DN%26height_min%3D0%26height_max%3D99%26year_min%3D{0}%26year_max%3D{1}%26birth_country_is%3DY%26as_comp%3Dgt%26as_val%3D0%26pos_is_g%3DY%26pos_is_gf%3DY%26pos_is_f%3DY%26pos_is_fg%3DY%26pos_is_fc%3DY%26pos_is_c%3DY%26pos_is_cf%3DY%26qual%3Dpts_per_g_req%26c1stat%3Dper%26c1comp%3Dgt%26c1val%3D-100%26c2stat%3Dws%26c2comp%3Dgt%26c2val%3D-100%26c3stat%3Dbpm%26c3comp%3Dgt%26c3val%3D-100%26c4stat%3Dusg_pct%26c4comp%3Dgt%26order_by%3Dws&div=div_stats".format(YEAR, YEAR))
+    first_100_content = BeautifulSoup(first_100.text, "html.parser")
+    first_table = first_100_content.find("tbody")
+    first_rows = first_table.findAll("tr")
+
+    if len(first_rows) > 99:
+        second_100 = requests.get("https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fplay-index%2Fpsl_finder.cgi%3Frequest%3D1%26match%3Dsingle%26type%3Dtotals%26per_minute_base%3D36%26per_poss_base%3D100%26season_start%3D1%26season_end%3D1%26lg_id%3DNBA%26age_min%3D0%26age_max%3D99%26is_playoffs%3DN%26height_min%3D0%26height_max%3D99%26year_min%3D{0}%26year_max%3D{1}%26birth_country_is%3DY%26as_comp%3Dgt%26as_val%3D0%26pos_is_g%3DY%26pos_is_gf%3DY%26pos_is_f%3DY%26pos_is_fg%3DY%26pos_is_fc%3DY%26pos_is_c%3DY%26pos_is_cf%3DY%26qual%3Dpts_per_g_req%26c1stat%3Dper%26c1comp%3Dgt%26c1val%3D-100%26c2stat%3Dws%26c2comp%3Dgt%26c2val%3D-100%26c3stat%3Dbpm%26c3comp%3Dgt%26c3val%3D-100%26c4stat%3Dusg_pct%26c4comp%3Dgt%26order_by%3Dws&offset%3D100&div=div_stats".format(YEAR, YEAR))
+        second_100_content = BeautifulSoup(second_100.text, "html.parser")
+        second_table = second_100_content.find("tbody")
+        second_rows = second_table.findAll("tr")
+
+        all_br_rows = first_rows + second_rows
+    else:
+        all_br_rows = first_rows
+
+    for row in all_br_rows:
+        try:
+            player_team = row.find("td", {"data-stat": "team_id"}).text
+        except AttributeError:
+            continue;
+
+        # accounting for special cases where NBA's tricode for a team differs
+        # slightly from basketball references'
+        if player_team == "PHX":
+            team_short = "PHO"
+        elif player_team == "CHA" and int(YEAR) >= 2015:
+            team_short = "CHO"
+        elif player_team =="BKN":
+            team_short = "BRK"
+        else:
+            team_short = player_team
+
+        player_name = ''
+        if row.find("td", {"data-stat": "player"}).text == 'Mohamed Bamba':
+            player_name = 'Mo Bamba'
+        else:
+            player_name = row.find("td", {"data-stat": "player"}).text
+
+        games = int(row.find("td", {"data-stat": "g"}).text)
+        minutes = int(row.find("td", {"data-stat": "mp"}).text)
+        points = int(row.find("td", {"data-stat": "pts"}).text)
+        rebounds = int(row.find("td", {"data-stat": "trb"}).text)
+        assists = int(row.find("td", {"data-stat": "ast"}).text)
+        steals = int(row.find("td", {"data-stat": "stl"}).text)
+        blocks = int(row.find("td", {"data-stat": "blk"}).text)
+        rookie = {
+            "player": player_name,
+            "team": "", # this comes from basketball reference standings scrape later
+            "team_short": player_team,
+            "wins": 0,
+            "team_games": 0, # this comes from basketball reference later
+            "player_games": row.find("td", {"data-stat": "g"}).text,
+            # advanced metrics get added via basketball reference later
+            "metrics": {
+                "winshare": float(row.find("td", {"data-stat": "ws"}).text),
+                "per": float(row.find("td", {"data-stat": "per"}).text),
+                "usg": float(row.find("td", {"data-stat": "usg_pct"}).text),
+                "bpm": float(row.find("td", {"data-stat": "bpm"}).text),
+                "mpg": round((minutes / games), 1),
+                "ppg": round((points / games), 1), # this comes from nba scrape later
+                "rpg": round((rebounds / games), 1), # this comes from nba scrape later
+                "apg": round((assists / games), 1), # this comes from nba scrape later
+                "spg": round((steals / games), 1), # this comes from nba scrape later
+                "bpg": round((blocks / games), 1) # this comes from nba scrape later
+            },
+            # zscores get calculated later
+            "zscores": {
+                "winshare": 0,
+                "per": 0,
+                "usg": 0,
+                "bpm": 0,
+                "mpg": 0,
+                "ppg": 0,
+                "rpg": 0,
+                "apg": 0,
+                "spg": 0,
+                "bpg": 0
+            }
+        }
+
+        rookie_data["players"].append(rookie)
+
+
+
+# ------------------------------------------------------------------------------
+# GETTING THE NBA STANDINGS SO WE CAN GET TEAM GAMES AND WINS
+
+# get the current nba standings so we can add in team games and team names to the
+# player dictionaries later
+
+def get_standings(year):
+    # getting the expanded standings broken down into rows
+    standings_request = requests.get("https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fleagues%2FNBA_{0}_standings.html&div=div_expanded_standings".format(year))
+    standings_content = BeautifulSoup(standings_request.text, "html.parser")
+    standings_table = standings_content.find("table", {"id": "expanded_standings"})
+    standings_rows = standings_table.find("tbody").findAll("tr")
+
+    # for each row ...
+    for row in standings_rows:
+        try:
+            # determine the team's record, wins and losses, wins games and team abbreviation
+            record = row.find("td", {"data-stat": "Overall"}).text
+            wins_losses = record.split("-")
+            wins = int(wins_losses[0])
+            games = int(wins_losses[0]) + int(wins_losses[1])
+            link = row.find("td", {"data-stat": "team_name"}).find("a").get("href")
+            split_link = link.split("teams/")
+            team_short = split_link[1].split("/")[0]
+
+            # create the team object
+            team = {}
+            team["team_name"] = row.find("td", {"data-stat": "team_name"}).text
+            team["record"] = record
+            team["wins"] = wins
+            team["games"] = games
+            team["short_name"] = team_short
+
+            # append it to the standings list
+            standings[year].append(team)
+        except AttributeError:
+            continue
+
+# ------------------------------------------------------------------------------
+# CROSS WALKING PLAYERS WITH TEAMS
+# function matches players with long team names and total games played by team
+
+def assign_games(players, standings):
+    # iterate over the players
+    for player in players:
+        # pull that player's tricode from their dictionary
+        player_team = player["team_short"]
+        # finding the team with the matching tricode in the standings
+        try:
+            team = next((team for team in standings if team["short_name"] == player_team))
+        except StopIteration:
+            print("Alert!!!: ", player_team, player)
+        # assign long team name and team games to player
+        player["team"] = team["team_name"]
+        player["team_games"] = team["games"]
+
+
+def get_traditional_stats(year):
 
     # the payload needed for the api request
     payload = {
@@ -128,163 +271,25 @@ def get_player_set(year):
     # converting the response to json
     nba_player_data = r.json()
 
-    # itertating over each player in the response and creating a rookie dictionary
-    # that gets added to our rookie_data dictionary defined above
-    for player in nba_player_data["resultSets"][0]["rowSet"]:
 
-        # accounting for special cases where NBA's tricode for a team differs
-        # slightly from basketball references'
-        if player[3] == "PHX":
-            team_short = "PHO"
-        elif player[3] == "CHA":
-            team_short = "CHO"
-        elif player[3] =="BKN":
-            team_short = "BRK"
-        else:
-            team_short = player[3]
+    for player in rookie_data["players"]:
 
-        # creating the rookie dictionary
-
-        player_name = ''
-        if player[1] == 'Mo Bamba':
-            player_name = 'Mohamed Bamba'
-        else:
-            player_name = player[1]
-        rookie = {
-            "player": player_name,
-            "team": "", # this comes from basketball reference later
-            "team_short": team_short,
-            "wins": player[6],
-            "team_games": 0, # this comes from basketball reference later
-            "player_games": player[5],
-            # advanced metrics get added via basketball reference later
-            "metrics": {
-                "winshare": 0,
-                "per": 0,
-                "usg": 0,
-                "vorp": 0,
-                "ppg": player[29],
-                "rpg": player[21],
-                "apg": player[22],
-                "spg": player[24],
-                "bpg": player[25]
-            },
-            # zscores get calculated later
-            "zscores": {
-                "winshare": 0,
-                "per": 0,
-                "usg": 0,
-                "vorp": 0,
-                "ppg": 0,
-                "rpg": 0,
-                "apg": 0,
-                "spg": 0,
-                "bpg": 0
-            }
-        }
-
-        rookie_data["players"].append(rookie)
-
-
-# ------------------------------------------------------------------------------
-# GETTING THE NBA STANDINGS SO WE CAN GET TEAM GAMES AND WINS
-
-# get the current nba standings so we can add in team games and team names to the
-# player dictionaries later
-
-def get_standings(year):
-    # getting the expanded standings broken down into rows
-    standings_request = requests.get("https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fleagues%2FNBA_{0}_standings.html&div=div_expanded_standings".format(year))
-    standings_content = BeautifulSoup(standings_request.text, "html.parser")
-    standings_table = standings_content.find("table", {"id": "expanded_standings"})
-    standings_rows = standings_table.find("tbody").findAll("tr")
-
-    # for each row ...
-    for row in standings_rows:
-
-        # determine the team's record, wins and losses, wins games and team abbreviation
-        record = row.find("td", {"data-stat": "Overall"}).text
-        wins_losses = record.split("-")
-        wins = int(wins_losses[0])
-        games = int(wins_losses[0]) + int(wins_losses[1])
-        link = row.find("td", {"data-stat": "team_name"}).find("a").get("href")
-        split_link = link.split("teams/")
-        team_short = split_link[1].split("/")[0]
-
-        # create the team object
-        team = {}
-        team["team_name"] = row.find("td", {"data-stat": "team_name"}).text
-        team["record"] = record
-        team["wins"] = wins
-        team["games"] = games
-        team["short_name"] = team_short
-
-        # append it to the standings list
-        standings[year].append(team)
-
-
-# ------------------------------------------------------------------------------
-# CROSS WALKING PLAYERS WITH TEAMS
-# function matches players with long team names and total games played by team
-
-def assign_games(players, standings):
-    # iterate over the players
-    for player in players:
-        # pull that player's tricode from their dictionary
-        player_team = player["team_short"]
-        # finding the team with the matching tricode in the standings
-        team = next((team for team in standings if team["short_name"] == player_team))
-        # assign long team name and team games to player
-        player["team"] = team["team_name"]
-        player["team_games"] = team["games"]
-
-
-# ------------------------------------------------------------------------------
-# WEEDING OUT THE PLAYERS WHO DON'T MEET THE GAMES PLAYED THRESHOLD FOR SCORING
-
-# function to weed out players from our data that don't meet the minimum games played
-# threshold of 70 percent of their team's games
-
-def weed_players(players):
-    # iterate over the players list from end to beginning
-    for i in range(len(players) - 1, -1, -1):
-        player = players[i]
-        # if the player's games played is not 70% of the team's games played, remove that player
-        if (player["player_games"] / player["team_games"]) * 100 < 70:
-            del players[i]
-
-
-def get_advance_metrics(players):
-
-    first_100 = requests.get("https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fplay-index%2Fpsl_finder.cgi%3Frequest%3D1%26match%3Dsingle%26type%3Dtotals%26per_minute_base%3D36%26per_poss_base%3D100%26season_start%3D1%26season_end%3D1%26lg_id%3DNBA%26age_min%3D0%26age_max%3D99%26is_playoffs%3DN%26height_min%3D0%26height_max%3D99%26year_min%3D2019%26year_max%3D2019%26birth_country_is%3DY%26as_comp%3Dgt%26as_val%3D0%26pos_is_g%3DY%26pos_is_gf%3DY%26pos_is_f%3DY%26pos_is_fg%3DY%26pos_is_fc%3DY%26pos_is_c%3DY%26pos_is_cf%3DY%26qual%3Dmp_per_g_req%26c1stat%3Dusg_pct%26c1comp%3Dgt%26c1val%3D0%26c2stat%3Dper%26c2comp%3Dgt%26c2val%3D-10%26c3stat%3Dws%26c3comp%3Dgt%26c3val%3D-10%26c4stat%3Dvorp%26c4comp%3Dgt%26c4val%3D-10%26c5val%3D.7%26order_by%3Dws&div=div_stats")
-
-    second_100 = requests.get("https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fplay-index%2Fpsl_finder.cgi%3Frequest%3D1%26match%3Dsingle%26type%3Dtotals%26per_minute_base%3D36%26per_poss_base%3D100%26season_start%3D1%26season_end%3D1%26lg_id%3DNBA%26age_min%3D0%26age_max%3D99%26is_playoffs%3DN%26height_min%3D0%26height_max%3D99%26year_min%3D2019%26year_max%3D2019%26birth_country_is%3DY%26as_comp%3Dgt%26as_val%3D0%26pos_is_g%3DY%26pos_is_gf%3DY%26pos_is_f%3DY%26pos_is_fg%3DY%26pos_is_fc%3DY%26pos_is_c%3DY%26pos_is_cf%3DY%26qual%3Dmp_per_g_req%26c1stat%3Dusg_pct%26c1comp%3Dgt%26c1val%3D0%26c2stat%3Dper%26c2comp%3Dgt%26c2val%3D-10%26c3stat%3Dws%26c3comp%3Dgt%26c3val%3D-10%26c4stat%3Dvorp%26c4comp%3Dgt%26c4val%3D-10%26c5val%3D.7%26order_by%3Dws&%3D%26offset%3D100&div=div_stats")
-
-    first_100_content = BeautifulSoup(first_100.text, "html.parser")
-    second_100_content = BeautifulSoup(second_100.text, "html.parser")
-
-    first_table = first_100_content.find("tbody")
-    second_table = second_100_content.find("tbody")
-
-    first_rows = first_table.findAll("tr")
-    try:
-        second_rows = second_table.findAll("tr")
-    except AttributeError:
-        second_rows = []
-
-    all_br_rows = first_rows + second_rows
-
-    for player in players:
-        for row in all_br_rows:
+        hit = False;
+        for row in nba_player_data["resultSets"][0]["rowSet"]:
             try:
-                if fuzz.token_sort_ratio(player["player"], row.find("td", {"data-stat": "player"}).text) >= 80:
-                    print(player["player"], row.find("td", {"data-stat": "player"}).text)
-                    player["metrics"]["winshare"] = float(row.find("td", {"data-stat": "ws"}).text)
-                    player["metrics"]["per"] = float(row.find("td", {"data-stat": "per"}).text)
-                    player["metrics"]["usg"] = float(row.find("td", {"data-stat": "usg_pct"}).text)
-                    player["metrics"]["vorp"] = float(row.find("td", {"data-stat": "vorp"}).text)
+                if fuzz.token_sort_ratio(player["player"], row[1]) >= 90:
+                    player["metrics"]["ppg"] = row[29]
+                    player["metrics"]["rpg"] = row[21]
+                    player["metrics"]["apg"] = row[22]
+                    player["metrics"]["spg"] = row[24]
+                    player["metrics"]["bpg"] = row[25]
+                    player["metrics"]["mpg"] = row[9]
+                    hit = True
             except AttributeError:
                 continue
+
+        if hit == False:
+            print('!!!!!ALERT!!!!!', player)
 
 
 def calculate_z_scores(data):
@@ -299,7 +304,7 @@ def calculate_z_scores(data):
     for player in data["players"]:
         player["total_stand_zscore"] = player["zscores"]["ppg"] + player["zscores"]["rpg"] + player["zscores"]["apg"] + player["zscores"]["spg"] + player["zscores"]["bpg"]
 
-        player["total_adv_zscore"] = player["zscores"]["winshare"] + player["zscores"]["vorp"] + player["zscores"]["per"] + player["zscores"]["usg"]
+        player["total_adv_zscore"] = player["zscores"]["mpg"] + player["zscores"]["winshare"] + player["zscores"]["bpm"] + player["zscores"]["per"] + player["zscores"]["usg"]
 
     data["players"] = sorted(data["players"], key=lambda x: (
         x['total_adv_zscore'], x['total_stand_zscore']))
@@ -307,8 +312,8 @@ def calculate_z_scores(data):
 
 def perform_scrape():
 
-    # get the initial player set
-    get_player_set(SEASON)
+    # get the advanced metrics for our remaining players
+    get_player_set()
 
     # get that season's corresponding team standings
     get_standings(YEAR)
@@ -316,11 +321,8 @@ def perform_scrape():
     # add team games to the player dictionaries
     assign_games(rookie_data["players"], standings[YEAR])
 
-    # filter our player set down to those who have played 70% of their team's games
-    weed_players(rookie_data["players"])
-
-    # get the advanced metrics for our remaining players
-    get_advance_metrics(rookie_data["players"])
+    # get the initial player set
+    # get_traditional_stats(SEASON)
 
     # calculate the standard deviation and mean for each metric
     set_stats(rookie_data)
